b0d73ab3d69b7704fda06b7abf4278fae1000560
TealishGreen <greenshirthead@icloud.com>
2026-01-11 17:18:47 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import os
 from dataclasses import dataclass
-from flask import Flask, jsonify, request, Response
+from flask import Flask, jsonify, Response, request
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime, timedelta, date
+from datetime import datetime, timedelta, date, timezone
 from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import DeclarativeMeta
 
 import uuid
 import json
 
-actions = ["Place_Block", "Break_Block", "Place_BlockWE", "Break_BlockWE", "Debug_Stick", "Container_Add", "Container_Remove"]
+actions = ["Place_Block", "Break_Block", "World_Edit", "Debug_Stick", "Rollback"]
 
 reverseActions = ["Break_Block", "Place_Block", "Break_BlockWE", "Place_BlockWE", "Debug_Stick", "Container_Remove", "Container_Add"]
 
@@ -18,7 +18,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
 app.secret_key = "67yu7uy6h j7"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True  
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 
 db = SQLAlchemy(app)
 
@@ -65,63 +65,97 @@ class Block(db.Model):
 
     def __repr__(self):
         return f'<Block {self.locationX} {self.locationY} {self.locationZ} {self.modifier} {self.modifiedAt} {self.id}>'
-    
-    
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "locationX": self.locationX,
+            "locationY": self.locationY,
+            "locationZ": self.locationZ,
+            "material": self.material,
+            "state": self.state,
+            "modifier": self.modifier,
+            "action": self.action,
+            "modifiedAt": self.modifiedAt.isoformat()
+        }
+
+
 @app.route('/')
 def index():
     return "y'all need to stop with this softcoding stuff: You're generally referring to removing repeated code – and yes, this a good thing, normally. However, there are two issues with the attitude around this. First, not every situation mandates this kind of code structure. This is especially true of smaller games made by newer developers. We shouldn't avoid adding certain QOL features simply because in a \"properly\" coded game it isn't necessary. This second point doesn't apply specifically to this conversation but I see it coming up frequently so here goes: a lot of people take this way too far. I have seen so many plots where they do crazy things like spawn items on the ground to load information and whatnot. This is not good. Don't sacrifice code legibility and efficiency simply because they believe it makes coding experience easier. There is a delicate balance between well designed, easy to maintain, and efficient code. Softcoding is not always the way to achieve this balance. So please, just think about the systems you're designing and whether or not anything is actually improved. tl;dr elitism through \"better practice\" / softcoding doesn't help, and often it isn't the right answer"
 
 @app.route('/version/')
 def version():
-    return jsonify({version: "2.0"})
+    return Response(json.dumps("2.0"), mimetype='application/json')
 
-@app.route('/get/<int:x>|<int:y>|<int:z>/')
+@app.route('/get/<int:x>/<int:y>/<int:z>/')
 def student(x, y, z):
-    student = Block.query.filter(Block.locationX == x, Block.locationY == y, Block.locationZ == z)
-    student = student[0]
-    return json.dumps({'success':True}), 200, jsonify({"modifier": student.modifier, "date": student.modifiedAt}), {'ContentType':'application/json'}
+    student = Block.query.filter(Block.locationX == x, Block.locationY == y, Block.locationZ == z).first()
+    print(student)
+    return Response(json.dumps(student, cls=AlchemyEncoder), mimetype='application/json')
+
 
 @app.route('/getbefore/<int:date>')
 def getbefore(date):
-    blocks = Block.query.filter(Block.modifiedAt <= datetime.utcfromtimestamp(date)).all()
+    blocks = Block.query.filter(Block.modifiedAt <= datetime.fromtimestamp(date, timezone.utc)).all()
     print(blocks)
     return Response(json.dumps(blocks, cls=AlchemyEncoder), mimetype='application/json')
 
-@app.route('/getrollbackdata/<int:date>|<modifier>', methods=("GET"))
-def getafter(date, modifier):
-    blocks = json.dumps(Block.filter(Block.modifiedAt >= datetime.utcfromtimestamp(date), Block.modifier == modifier), cls=AlchemyEncoder)
-    db.session.commit()
-    return Response(blocks, mimetype='application/json')
+@app.route('/getrollbackdata/<int:date>/<modifier>', methods=["GET"])
+def getrollbackdata(date, modifier):
+    dt = datetime.fromtimestamp(date, timezone.utc)
+    print(Block.query.filter(
+        Block.modifiedAt >= dt,
+        Block.modifier == modifier
+    ).all())
+    blocks = Block.query.filter(
+        Block.modifiedAt >= dt,
+        Block.modifier == modifier
+    ).all()
 
-@app.route('/delrollbackdata/<int:date>|<modifier>', methods=("DELETE"))
-def getafter(date, modifier):
-    blocks = json.dumps(Block.filter(Block.modifiedAt >= datetime.utcfromtimestamp(date), Block.modifier == modifier), cls=AlchemyEncoder)
+    return Repsonse(json.dumps(blocks, cls=AlchemyEncoder), mimetype='application/json')
+@app.route('/delrollbackdata/<int:date>/<modifier>', methods=["DELETE"])
+def delafter(date, modifier):
     db.session.execute(
         db.delete(Block).filter(Block.modifiedAt >= datetime.utcfromtimestamp(date), Block.modifier == modifier)
     )
     db.session.commit()
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-@app.route('/deleteold', methods=('DELETE'))
+@app.route('/deleteold', methods=['DELETE'])
 def deleteold():
     db.session.execute(
-        db.delete(Block).filter(Block.modifiedAt <= date.today() - timedelta(20))
+        db.delete(Block).filter(Block.modifiedAt <= date.today() - timedelta(2))
     )
     db.session.commit()
     return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
-@app.route('/create/<x>|<y>|<z>|<material>|<state>|<modifier>|<int:action>|<date>/', methods=('POST'))
-def create(x, y, z, material, state, modifier, action, date):
-    id = str(uuid.uuid4())
-    x = int(x)
-    y = int(y)
-    z = int(z)
-    action = actions[action]
-    date = datetime.utcfromtimestamp(float(date))
-    student = Block(locationX=x, locationY=y, locationZ=z, material=material, state=state, modifier=modifier, action=action, modifiedAt=date, id=id)
-    db.session.add(student)
-    db.session.commit()
-    return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
+@app.route('/create/', methods=['POST'])
+def create():
+    data = request.get_json()
+    print(data)
+    new_blocks = [
+        Block(
+            id=str(uuid.uuid4()),
+            locationX=int(json.loads(item)['x']),
+            locationY=int(json.loads(item)['y']),
+            locationZ=int(json.loads(item)['z']),
+            material=json.loads(item)['material'],
+            state=json.loads(item)['state'],
+            modifier=json.loads(item)['modifier'],
+            action=actions[int(json.loads(item)['action'])],
+            modifiedAt=datetime.utcfromtimestamp(float(json.loads(item)['date']))
+        ) for item in data
+    ]
+
+    try:
+        db.session.add_all(new_blocks)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()  # Always rollback on IntegrityError
+        raise e
+
+    return {'you_sent': data}, 200
 
 @app.route("/checkconn")
 def checkConn():
